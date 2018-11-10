3b37988123330e1dff39cb06023c80142cb88cbb
eb247 <eb247@hotmail.com>
2018-11-10 02:43:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nowServing(katzDeliLine) {
   if(katzDeliLine.length < 1) {
     return "There is nobody waiting to be served!"
   } else {
-    return "Currently serving " +  katzDeliLine.shift(katzDeliLine[0]) + "."
+    return "Currently serving " +  katzDeliLine.shift(katzDeliLine[0]) + ".";
 
   }
 
