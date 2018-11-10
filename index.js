b40ef08b2c29b28fceb0d7c80c434d280831a958
eb247 <eb247@hotmail.com>
2018-11-10 01:17:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,14 +2,6 @@ function takeANumber(katzDeliLine, person){
   katzDeliLine.push(person);
   var arrayLength = katzDeliLine.length;
   var personPosition = 1 + katzDeliLine.indexOf(person);
-
-  if(katzDeliLine.length < 1) {
-    katzDeliLine[0] = person;
-  } else {
-    katzDeliLine.push(person);
-  }
-
-
   return `Welcome, ${person}. You are number ${personPosition} in line.`;
 }
 
