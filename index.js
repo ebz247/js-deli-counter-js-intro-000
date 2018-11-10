6ead0e05fc2a8c1decb044b5806b51e6863ab9c6
eb247 <eb247@hotmail.com>
2018-11-10 02:41:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function nowServing(katzDeliLine) {
     return "There is nobody waiting to be served!"
   } else {
     return "Currently serving " + katzDeliLine;
-    katzDeliLine.shift(katzDeliLine);
+    katzDeliLine.shift(katzDeliLine[0]);
   }
 
 }
