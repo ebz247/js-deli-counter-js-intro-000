10a8be9f087a355b5197448eecd3ca34a57d6da8
eb247 <eb247@hotmail.com>
2018-11-10 02:35:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function nowServing(katzDeliLine) {
   } else {
     return Currently serving " + katzDeliLine[0] + ".";
   }
+  katzDeliLine.shift(katzDeliLine[0])
 }
 
 function currentLine(katzDeliLine){
